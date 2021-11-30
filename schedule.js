2683907fff35199e3f0346bd872ac60211586fe0
Jesper Falk <jesper.falk@iotopen.se>
2021-11-30 09:31:07 +0100

    Added API calls for Roles and Permissions

--- schedule.js
+++ schedule.js
@@ -2,7 +2,7 @@ import {Endpoints, request} from "./util";
 
 export const GetSchedules = (installationId, executor) => {
     let qs = executor ? '?executor=' + executor : '';
-    return request(Endpoints.Schedule + installationId + qs);
+    return request(Endpoints.Schedule + '/' + installationId + qs);
 }
 
 export const GetSchedule = (installationId, id) => request(Endpoints.Schedule + '/' + installationId + '/' + id, {});
