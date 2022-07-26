52016e9117c4fec7368eb14768ff578e5b3bbb90
Jesper Falk <jesper.falk@iotopen.se>
2022-07-26 14:17:15 +0200

    Added API:s for Files

---
+++ file.js
@@ -0,0 +1,66 @@
+import {Endpoints, request} from "./util";
+import {re} from "@babel/core/lib/vendor/import-meta-resolve";
+
+export const GetFilesInstallation = (installationId) => {
+    return request(Endpoints.File + '/installation/' + installationId, {});
+}
+
+export const GetFileInstallation = (installationId, fileId) => {
+    return request(Endpoints.File + '/installation/' + installationId + '/' + fileId, {});
+}
+
+export const CreateFileInstallation = (installationId, file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+    return request(Endpoints.File + '/installation/' + installationId, {
+        method: 'POST', body: formData
+    });
+}
+
+export const UpdateFileInstallation = (installationId, fileId, file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+    return request(Endpoints.File + '/installation/' + installationId + '/' + fileId, {
+        method: 'PUT', body: formData
+    });
+}
+
+export const DeleteFileInstallation = (installationId, fileId) => {
+    return request(Endpoints.File + '/installation/' + installationId + '/' + fileId, {
+        method: 'DELETE'
+    });
+}
+
+export const GetFilesOrganization = (organizationId) => {
+    return request(Endpoints.File + '/organization/' + organizationId, {});
+}
+
+export const GetFileOrganization = (organizationId, fileId) => {
+    return request(Endpoints.File + '/organization/' + organizationId + '/' + fileId, {});
+}
+
+export const CreateFileOrganization = (organizationId, file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+    return request(Endpoints.File + '/organization/' + organizationId, {
+        method: 'POST', body: formData
+    });
+}
+
+export const UpdateFileOrganization = (organizationId, fileId, file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+    return request(Endpoints.File + '/organization/' + organizationId + '/' + fileId, {
+        method: 'PUT', body: formData
+    });
+}
+
+export const DeleteFileOrganization = (organizationId, fileId) => {
+    return request(Endpoints.File + '/organization/' + organizationId + '/' + fileId, {
+        method: 'DELETE'
+    });
+}
+
+export const DownloadFile = (hash) => {
+    return request(Endpoints.File + '/download/' + hash);
+}
