6273d49f010749cd027511b57fcd2f3388c634a4
Tobias Assarsson <tobias.assarsson@gmail.com>
2022-11-24 13:20:15 +0100

    log

--- src/log.ts
+++ src/log.ts
@@ -15,17 +15,17 @@ export enum LogOrder {
     Asc = 'asc'
 }
 
-export const GetStatus = (installationId: number, topicFilter?: string[]) => {
-    const qs = topicFilter ? '?' + topicFilter.reduce((prev, cur, id) => {
+export function GetStatus(installationId: number, topicFilter?: string[]) {
+    const qs = topicFilter ? `?${topicFilter.reduce((prev, cur, id) => {
         if (id !== 0) {
             prev += '&';
         }
-        return prev + 'topics=' + cur;
-    }, '') : '';
-    return request<LogEntry[]>(Endpoints.Status + '/' + installationId + qs, {});
-};
+        return `${prev}topics=${cur}`;
+    }, '')}` : '';
+    return request<LogEntry[]>(`${Endpoints.Status}/${installationId}${qs}`, {});
+}
 
-export const GetLog = (installationId: number, from?: number, to?: number, limit = 500, offset = 0, order = LogOrder.Desc, topics?: string[]) => {
+export function GetLog (installationId: number, from?: number, to?: number, limit = 500, offset = 0, order = LogOrder.Desc, topics?: string[]) {
     const now = new Date().getTime() / 1000;
     from = from ? from : now - (60 * 60 * 24);
     to = to ? to : now;
@@ -37,6 +37,6 @@ export const GetLog = (installationId: number, from?: number, to?: number, limit
     if (topics) {
         params.topics = topics.join(',');
     }
-    const qs = '?' + new URLSearchParams(params).toString();
-    return request<PaginatedResponse<LogEntry>>(Endpoints.LogV3 + '/' + installationId + qs, {});
-};
+    const qs = `?${new URLSearchParams(params).toString()}`;
+    return request<PaginatedResponse<LogEntry>>(`${Endpoints.LogV3}/${installationId}${qs}`, {});
+}
