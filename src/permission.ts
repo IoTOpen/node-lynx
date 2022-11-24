085eb8a51b6a684dc2ae07fb216b50bc599452aa
Tobias Assarsson <tobias.assarsson@gmail.com>
2022-11-24 13:26:26 +0100

    permission

--- src/permission.ts
+++ src/permission.ts
@@ -1,3 +1,5 @@
 import {Endpoints, request} from './util';
 
-export const GetPermissions = () => request<{[key: string]: boolean}>(Endpoints.Permission, {});
+export function GetPermissions () {
+    return request<{[key: string]: boolean}>(Endpoints.Permission, {});
+}
