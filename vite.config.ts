f78fb0b32d311345f7b85c3f676d1cef1100e81b
Felix Jontegen <felix.jontegen@th1ng.se>
2026-05-07 10:49:42 +0200

    fix miss in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
       formats: ['cjs', 'es'],
       fileName: (format) => (format === 'cjs' ? 'index.cjs' : 'index.js'),
     },
-    rollupOptions: {
+    rolldownOptions: {
       external: isExternal,
     },
   },
